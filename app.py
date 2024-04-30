e5411eaefcfb6193cae5a8fbf33a41962c6d8694
attahiruj <attahiruj@gmail.com>
2024-04-30 15:27:56 +0100

    flake 8 enforcement structuring

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from flask import Flask, jsonify, request
-from model import *
+from models import predict_class
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
-@app.route('/predict', methods = ['POST', 'GET'])
+@app.route('/predict', methods=['POST', 'GET'])
 def predict():
-	if request.method == 'POST':
-		json = request.get_json()
-		print(json['name'])
-		prediction = predict_class(json['name'])
-		return jsonify({'language' : prediction})
-	else:
-		return 'Hello World!'
+    if request.method == 'POST':
+        json = request.get_json()
+        print(json['name'])
+        prediction = predict_class(json['name'])
+        return jsonify({'language': prediction})
+    else:
+        return 'Hello World!'
+
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
